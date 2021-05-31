00b9f752a3eabd24f277e0cf263be220192541f4
BILL <valygnos@gmail.com>
2021-05-31 18:05:38 +0300

    Updates

---
+++ precache-manifest.e3ab93c4f991253b8ac37d9c4f72c3e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "309e75e7ff06bffeb73ed0cb66c60e55",
+    "url": "/dappnew/index.html"
+  },
+  {
+    "revision": "e4bf5413dccfc05fe070",
+    "url": "/dappnew/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "03ccc7724ad24631bf01",
+    "url": "/dappnew/static/js/2.184f034c.chunk.js"
+  },
+  {
+    "revision": "e4bf5413dccfc05fe070",
+    "url": "/dappnew/static/js/main.1e857fca.chunk.js"
+  },
+  {
+    "revision": "1677c123b49e15dd7cf4",
+    "url": "/dappnew/static/js/runtime-main.5caa8c20.js"
+  }
+]);
